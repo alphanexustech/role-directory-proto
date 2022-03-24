431129c6b6d47bd32153d235419b54a6526fe0f2
Jack Poulsen <johnpoulsenjr@gmail.com>
2022-03-24 15:25:24 -0400

    chore: Add test data

---
+++ src/role_dir.py
@@ -0,0 +1,20 @@
+test_data_lookup = {
+    "programmer": {
+        "key_words": ["python", "javascript", "software"]
+    }
+}
+
+sample_statement = """
+As a softwared developer, my programs infterface with a variety of systems and complex
+"""
+
+print(test_data_lookup)
+print(sample_statement)
+
+# Write a function with one input string (api interface)
+
+
+# Find key words in the text with string matching (search)
+
+
+# Return roles that match the text from a lookup object (query results)
