34d5646e143b84945be52975c221318ed0649b56
Jack Poulsen <johnpoulsenjr@gmail.com>
2022-03-25 19:48:28 -0400

    add for loop with function

--- src/role_dir.py
+++ src/role_dir.py
@@ -5,21 +5,34 @@ test_data_lookup = {
 }
 
 sample_statement = """
-As a softwared developer, my programs infterface with a variety of systems and complex
+As a software developer, my programs infterface with a variety of systems and complex
 """
 
-print(test_data_lookup)
-print(sample_statement)
+# print(test_data_lookup)
+# print(sample_statement)
 
 
 # Write a function with one input string (api interface)
 def sample_statement_handler(vrb):
-  print(vrb)
+    print(vrb)
 
+def basic_function():
+    print("hello mister anderson")
 
-sample_statement_handler(sample_statement)
+# sample_statement_handler(sample_statement)
 
 # Find key words in the text with string matching (search)
+# how do i write a for loop
+key_words = ["python", "javascript", "software"]
+for x in key_words:
+    print(x)
+    if x == "javascript":
+        basic_function()
+
+# how do i split a string on a specific character
+# for x in "python":
+#     print(x)
+
 
 
 # Return roles that match the text from a lookup object (query results)
