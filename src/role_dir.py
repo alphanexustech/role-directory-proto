adf7acff0302a72132f01ba59e53b854ca7cf9cc
Jack Poulsen <johnpoulsenjr@gmail.com>
2022-03-30 20:12:57 -0400

    split a string and for loop match

--- src/role_dir.py
+++ src/role_dir.py
@@ -5,7 +5,7 @@ test_data_lookup = {
 }
 
 sample_statement = """
-As a software developer, my programs infterface with a variety of systems and complex
+As a software developer, my programs infterface with a variety of systems, like javascript , python and complex
 """
 
 # print(test_data_lookup)
@@ -16,23 +16,24 @@ As a software developer, my programs infterface with a variety of systems and co
 def sample_statement_handler(vrb):
     print(vrb)
 
-def basic_function():
-    print("hello mister anderson")
+def basic_function(m):
+    print("match found", m)
 
 # sample_statement_handler(sample_statement)
 
 # Find key words in the text with string matching (search)
 # how do i write a for loop
 key_words = ["python", "javascript", "software"]
+string_list = sample_statement.split()
 for x in key_words:
-    print(x)
-    if x == "javascript":
-        basic_function()
-
-# how do i split a string on a specific character
-# for x in "python":
-#     print(x)
+    # print(x)
+    for z in string_list:
+        # print(z)
+        if x == z:
+            basic_function(z)
 
 
 
 # Return roles that match the text from a lookup object (query results)
+
+#
