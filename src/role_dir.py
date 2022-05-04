875f090300a808c856d250d60fdd1a3c55b742ac
Jack Poulsen <johnpoulsenjr@gmail.com>
2022-05-03 20:52:12 -0400

    Refactor python functions

--- src/role_dir.py
+++ src/role_dir.py
@@ -13,27 +13,24 @@ As a software developer, my programs infterface with a variety of systems, like 
 
 
 # Write a function with one input string (api interface)
-def sample_statement_handler(vrb):
-    print(vrb)
+def query_results(sample_statement):
+    # Find key words in the text with string matching (search)
+    key_words = ["python", "javascript", "software"]
+    string_list = sample_statement.split()
+    for x in key_words:
+        # print(x)
+        for z in string_list:
+            # print(z)
+            if x == z: # "word" == 'word'
+                role_finder(z)
 
-def basic_function(m):
+def role_finder(m):
     print("match found", m)
 
-# sample_statement_handler(sample_statement)
+query_results(sample_statement)
 
-# Find key words in the text with string matching (search)
-# how do i write a for loop
-key_words = ["python", "javascript", "software"]
-string_list = sample_statement.split()
-for x in key_words:
-    # print(x)
-    for z in string_list:
-        # print(z)
-        if x == z:
-            basic_function(z)
+
 
 
 
 # Return roles that match the text from a lookup object (query results)
-
-#
