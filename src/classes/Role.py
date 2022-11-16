7aba38e81a363dde60b3d0b33422560a119d91c3
Jack Poulsen <johnpoulsenjr@gmail.com>
2022-11-16 14:29:18 -0500

    finally got it

--- src/classes/Role.py
+++ src/classes/Role.py
@@ -5,9 +5,11 @@ class Role():
     set()
     # define getter and setters
 class Role():
-    def __init__(self, name = "First-Line Supervisors of Production and Operating Workers"):
+    def __init__(self, name = None):
         self.__name = name
 
     def get_name(self):
         return self.__name
-    print(__name)
+    
+    def set_name(self):
+        self.__name = "First-Line Supervisors of Production and Operating Workers"
