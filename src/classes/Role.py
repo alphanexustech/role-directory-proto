b370d9fbb8d6cd2d0e9ec716c97cd3c05d3ed4fc
Jack Poulsen <johnpoulsenjr@gmail.com>
2022-11-16 14:21:38 -0500

    first role name

--- src/classes/Role.py
+++ src/classes/Role.py
@@ -4,3 +4,10 @@ class Role():
 
     set()
     # define getter and setters
+class Role():
+    def __init__(self, name = "First-Line Supervisors of Production and Operating Workers"):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+    print(__name)
