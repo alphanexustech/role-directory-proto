35fa0b4dd90102a7d332cdf904b84fe00ac3a1a6
Jack Poulsen <johnpoulsenjr@gmail.com>
2022-11-16 15:07:52 -0500

    finally a function

--- src/classes/Role.py
+++ src/classes/Role.py
@@ -6,5 +6,13 @@ class Role():
     def get_name(self):
         return self.__name
     
-    def set_name(self):
-        self.__name = "First-Line Supervisors of Production and Operating Workers"
+    def set_name(self, x):
+        self.__name = x
+
+first_line_supervisors_of_roduction_and_operating_workers = Role
+
+first_line_supervisors_of_roduction_and_operating_workers.set__name("First-Line Supervisors of Production and Operating Workers")
+
+print(first_line_supervisors_of_roduction_and_operating_workers.get__name())
+
+print(first_line_supervisors_of_roduction_and_operating_workers.__name)
